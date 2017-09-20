1263dc0dd08fb84b160132353415a551609a172d
Jevon Wallace <jw0155039@otc.edu>
2017-09-20 13:25:09 -0500

    stuff

---
+++ index.js
@@ -0,0 +1,40 @@
+const inquirer = require("inquirer")
+const chalk = require('chalk')
+
+console.log(chalk.green("Hello, I'm IO Your Pizza Generator!"))
+console.log("Let me make some pizzas for you!")
+
+let questions = [{
+
+name: "number", 
+message:"How many Pizzas should I make for you? ",
+
+},{
+name: "size",
+type: "size", 
+message:"What size Pizza's do you like? ",
+choices:["S", "M", "L"],
+
+},{
+
+name: "toppings", 
+message:"What toppings do you like? ",
+
+},{
+
+name: "cut", 
+message:"How would you like me to cut your pizza? ",
+
+}]
+
+inquirer.prompt(questions).then(function({number, size, toppings, cut}){
+console.log("Your Order is complete,") 
+console.log("You Ordered " + number + " Pizza's")
+console.log("Pizza Size " + size)
+console.log("The topping(s) for your pizza are " + toppings)
+console.log("I will cut your pizza in the style of " + cut + " cut")
+})
+
+
+                                                                                                                               
+
